63a19216daebc82afb3687bd4b18a1be70c56f38
Rosano Coutinho <github.com@xeari.com>
2025-09-26 13:08:18 +0100

    spec home nav

--- src/routes/tests-ui.js
+++ src/routes/tests-ui.js
@@ -2,5 +2,15 @@ import { expect, test } from '@playwright/test';
 
 test('home page has expected h1', async ({ page }) => {
 	await page.goto('/');
-	await expect(page.locator('h1')).toBeVisible();
+	await expect(page.locator('h1')).toContainText('test-sveltekit');
+});
+
+test('navigation items', async ({ page }) => {
+	await page.goto('/');
+	await expect(page.locator('nav a:nth-child(1)')).toBeVisible();
+
+  await expect(page.getByRole('navigation')).toContainText('home');
+  await expect(page.getByRole('navigation')).toContainText('guide');
+  await expect(page.getByRole('navigation')).toContainText('redirect');
+  await expect(page.getByRole('navigation')).toContainText('app1');
 });
