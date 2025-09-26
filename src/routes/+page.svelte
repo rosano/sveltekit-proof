1360cd93d4e25609104a7b409b0df74699495112
Rosano Coutinho <github.com@xeari.com>
2025-09-26 12:52:31 +0100

    clarify intro

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,10 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<h1>test-sveltekit</h1>
+
+<p>Finding my building blocks for app development.</p>
+
+<p>Docs at <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a></p>
+
+<hr>
 
 # no markdown here
 <p>but maybe <a href="/guide">here?</a></p>
