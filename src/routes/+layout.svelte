ac2c032f19d2c1600802c29ad88af6d4cd3dba5f
Rosano Coutinho <github.com@xeari.com>
2025-09-29 10:51:44 +0100

    replace bullet

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -15,15 +15,15 @@ let { children } = $props();
 <header>
 	<nav>
 		<a href="/">home</a>
-		•
+		·
 		<a href="/markdown-page">markdown-page</a>
-		•
+		·
 		<a href="/redirect">redirect</a>
-		•
+		·
 		<a href="/app1">app1</a>
-		•
+		·
 		<a href="/app2">app2</a>
-		•
+		·
 		<a href="/hash-route">hash-route</a>
 	</nav>
 </header>
