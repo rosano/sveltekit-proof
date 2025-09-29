2197eb8aa657bb66e5f89db31c5264e7dd6fd3ca
Rosano Coutinho <github.com@xeari.com>
2025-09-29 10:38:04 +0100

    pass west

--- src/routes/tests-ui.js
+++ src/routes/tests-ui.js
@@ -6,12 +6,16 @@ test('heading', ({ page }) =>
 	expect(page.locator('h1')).toContainText('svp')
 );
 
-test('navigation items', async ({ page }) => {
-	await expect(page.locator('nav a:nth-child(1)')).toBeVisible();
+test('navigation items', ({ page }) =>
+  expect(page.locator('nav a')).toHaveText([
+    'home',
+    'markdown-page',
+    'redirect',
+    'app1',
+    'app2',
+    'hash-route',
+  ])
+);
 
-  await expect(page.getByRole('navigation')).toContainText('home');
-  await expect(page.getByRole('navigation')).toContainText('markdown-page');
-  await expect(page.getByRole('navigation')).toContainText('redirect');
-  await expect(page.getByRole('navigation')).toContainText('app1');
-  await expect(page.getByRole('navigation')).toContainText('app2');
+test('open-source', ({ page }) => {
 });
