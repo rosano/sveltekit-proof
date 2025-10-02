3ec114aea538cbb7ede32b4733655059d8d16f2d
Rosano Coutinho <github.com@xeari.com>
2025-10-02 18:02:52 +0100

    spec manifest

--- src/routes/app1/tests-ui.js
+++ src/routes/app1/tests-ui.js
@@ -2,6 +2,10 @@ import { expect, test } from '@playwright/test';
 
 test.beforeEach(({ page }) => page.goto('/app1'));
 
+test('manifest', ({ page }) =>
+	expect(page.locator('link[rel="manifest"]')).toHaveAttribute('href', '/manifest.json')
+);
+
 test('h1', ({ page }) =>
 	expect(page.locator('h1')).toContainText('SPA')
 );
