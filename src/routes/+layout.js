b6f0491f23a8cad11c26b194156e6670d3facc05
Rosano Coutinho <github.com@xeari.com>
2025-10-03 11:58:01 +0100

    extract sections

--- src/routes/+layout.js
+++ src/routes/+layout.js
@@ -1,2 +1,16 @@
 export const prerender = true;
 export const trailingSlash = 'always';
+
+/** @type {import('./$types').LayoutLoad} */
+export function load() {
+	return {
+		navigation: [
+			{ path: '/', title: 'home' },
+			{ path: '/markdown-page', title: 'markdown-page' },
+			{ path: '/redirect', title: 'redirect' },
+			{ path: '/app1', title: 'app1' },
+			{ path: '/app2', title: 'app2' },
+			{ path: '/hash-route', title: 'hash-route' },
+		],
+	};
+}
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,7 @@
 <script>
 import favicon from '$lib/assets/favicon.svg';
 
-let { children } = $props();
-
-// If you're using a fallback (i.e. SPA mode) you don't need to prerender all
-// pages by setting this here, but should prerender as many as possible to
-// avoid large performance and SEO impacts
+let { children, data } = $props();
 </script>
 
 <svelte:head>
@@ -14,17 +10,9 @@ let { children } = $props();
 
 <header>
 	<nav>
-		<a href="/">home</a>
-		·
-		<a href="/markdown-page">markdown-page</a>
-		·
-		<a href="/redirect">redirect</a>
-		·
-		<a href="/app1">app1</a>
-		·
-		<a href="/app2">app2</a>
-		·
-		<a href="/hash-route">hash-route</a>
+		{#each data.navigation as section, index}
+			{#if index}&nbsp;·&nbsp;{/if}<a href="{section.path}">{section.title}</a>
+		{/each}
 	</nav>
 </header>
 
