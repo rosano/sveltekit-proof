04209b23135934b1edcb4e88a948cbc0bcc7d4fb
Rosano Coutinho <github.com@xeari.com>
2025-10-09 13:08:08 +0100

    style toast

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -29,3 +29,25 @@ let { children, data } = $props();
 		<button onclick={() => location.reload()}>reload the page</button>
 	</div>
 {/if}
+
+<style>
+.toast {
+	position: fixed;
+	left: 0;
+	bottom: 0;
+	width: 100%;
+	padding: 1rem;
+	display: flex;
+	justify-content: center;
+	gap: 1rem;
+}
+
+.toast p {
+	display: flex;
+	align-items: center;
+	gap: 1rem;
+	background: var(--bg-2);
+	padding: 0.5rem 0.5rem 0.5rem 1rem;
+	border-radius: 4px;
+}
+</style>
